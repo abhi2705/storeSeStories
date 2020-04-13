89fe291ec5e905144a862e4090b24e0f72fa901a
Neeraj <neeraj.deshpande3@gmail.com>
2020-04-13 20:38:52 +0530

    Added new brands page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { FavouritesComponent } from './components/favourites/favourites.component';
-
-const routes: Routes = [ {
+import { BrandNavComponent } from './components/brand-nav/brand-nav.component';
+const routes: Routes = [
+  {
+    path: 'brands',
+    component: BrandNavComponent
+  },
+  {
   path: '',
   component: HomePageComponent
 },
-{
-  path: 'likes',
-  component: FavouritesComponent
-}
 ];
 
 @NgModule({

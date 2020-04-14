81eb7797bf4049611ba38d63f619a359f04c4a88
Neeraj <neeraj.deshpande3@gmail.com>
2020-04-14 19:57:21 +0530

    Need to make it dynamic

--- src/app/components/brand-nav/brand-nav.component.ts
+++ src/app/components/brand-nav/brand-nav.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BrandNavComponent implements OnInit {
   sidebarIsActive = false;
   constructor() { }
+  selectedTab = "All";
 
   ngOnInit(): void {
   }

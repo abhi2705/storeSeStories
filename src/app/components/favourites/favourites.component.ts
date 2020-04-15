8aef48d9ac4ca27f507f0202a20f8e14ec4ff3ef
Arpitha <arpitharaghunandan@gmail.com>
2020-04-15 19:24:43 +0530

    liked cards formatting

--- src/app/components/favourites/favourites.component.ts
+++ src/app/components/favourites/favourites.component.ts
@@ -3,6 +3,7 @@ import { ApiService } from '../../services/api.service';
 import { Observable } from 'rxjs';
 import { Stories } from 'src/app/models/story.model';
 import { DOCUMENT } from '@angular/common';
+import { ShareTabService } from 'src/app/services/share-tab.service';
 
 @Component({
   selector: 'app-favourites',
@@ -17,14 +18,29 @@ export class FavouritesComponent implements OnInit {
 
   source = "";
   target = "";
+  time = ""
+
+  liked = [];
+  like = ""
+  ind = 0;
 
   constructor(@Inject(DOCUMENT) private document: Document, 
-                                private apiservice: ApiService) { }
+                                private apiservice: ApiService,
+                                private shareTabService: ShareTabService) { }
 
   ngOnInit(): void {
     this.stories$ = this.apiservice.stories.get();
     this.stories$.subscribe((data: Stories) => {
       console.log(data);
+      var all_stories = data.stories;
+      var l = all_stories.length;
+      var i = 0;
+      for(i = 0; i < l; i++){
+        // if(all_stories[i].isLiked == true)
+          this.liked.push("like_y");
+        // else
+        //   this.liked.push("like_n");
+      }
     });
   }
 
@@ -37,10 +53,36 @@ export class FavouritesComponent implements OnInit {
     this.document.location.href = targetUrl;
   }
 
-  onClick(story) {
+  onClick(story, i) {
     this.source = story.imageUrl;
     this.target = story.targetUrl;
+    this.time = this.get_duration(story);
+    this.ind = i;
+    this.like = this.liked[i];
     this.document.getElementById("modal01").style.display = "block";
+  }
+
+  get_duration(story){
+    const date1 = new Date(story.postedAt);
+    const date2 = new Date();
+    const utc_date2 = new Date(date2.getTime() + date2.getTimezoneOffset() * 60000);
+    const diffTime = Math.abs(utc_date2.getTime() - date1.getTime());
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    const diffHours = Math.ceil(diffTime / (1000 * 60 * 60));
+    if (diffHours >= 24) {
+      return diffDays.toString() + ' d';
+    }
+    return diffHours.toString() + ' h';
+  }
+
+  activateShare(brandUrl: string) {
+    console.log('activating share');
+    this.shareTabService.activate(brandUrl);
+  }
+
+  remove_like(i){
+    this.liked[i] = "like_n";
+    return;
   }
 
 }

ffda5bf6d6e8edc1442c793f1cc2f60f61d4cf5e
Arpitha <arpitharaghunandan@gmail.com>
2020-04-23 11:54:42 +0530

    added button to liked page on stories page

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FavButtonService } from '../../services/fav-button.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,8 +11,16 @@ export class NavBarComponent implements OnInit {
 
   readonly logoImg = 'assets/img/logos/Storese_branding-01.png';
 
-  constructor() { }
+  fav_btn_enabled: boolean;
+  private sub: Subscription;
+
+  constructor(private favBtnService: FavButtonService) { }
 
   ngOnInit(): void {
+    this.sub = this.favBtnService.favEnabled.subscribe(enabled => this.fav_btn_enabled = enabled);
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }

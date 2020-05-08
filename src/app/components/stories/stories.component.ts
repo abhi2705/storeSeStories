57e867c77f05c74d6b403e72cd8c6adb70ea273e
yashmagarwal786 <yashmagarwal786@gmail.com>
2020-05-08 20:09:12 +0530

    Smooth transition of cards

--- src/app/components/stories/stories.component.ts
+++ src/app/components/stories/stories.component.ts
@@ -191,10 +191,12 @@ export class StoriesComponent implements OnInit, OnDestroy {
 
         swiper.params.watchSlidesProgress = true;
         swiper.originalParams.watchSlidesProgress = true;
+        // Uncommenting this will remove extra left and right swipe space from the first and the last card respectively
         // swiper.params.touchReleaseOnEdges = true;
         // swiper.originalParams.touchReleaseOnEdges = true;
         // swiper.passedParams.touchReleaseOnEdges = true;
       },
+
       setTranslate() {
         // console.log("translate");
         const swiper = this;
@@ -215,12 +217,12 @@ export class StoriesComponent implements OnInit, OnDestroy {
           const slideOffset = $slideEl[0].swiperSlideOffset;
           const offsetMultiplier = ((center - slideOffset - (slideSize / 2)) / slideSize) * params.modifier;
 
-           let rotateY = isHorizontal ? rotate * offsetMultiplier : 0;
+          let rotateY = isHorizontal ? rotate * offsetMultiplier : 0;
           let rotateX = isHorizontal ? 0 : rotate * offsetMultiplier;
           // var rotateZ = 0
           let translateZ = -translate * Math.abs(offsetMultiplier);
 
-           let translateY = isHorizontal ? 0 : params.stretch * (offsetMultiplier);
+          let translateY = isHorizontal ? 0 : params.stretch * (offsetMultiplier);
           let translateX = isHorizontal ? params.stretch * (offsetMultiplier) : 0;
 
            // Fix for ultra small values
@@ -230,25 +232,10 @@ export class StoriesComponent implements OnInit, OnDestroy {
           if (Math.abs(rotateY) < 0.001) rotateY = 0;
           if (Math.abs(rotateX) < 0.001) rotateX = 0;
 
-           const slideTransform = `translate3d(${translateX}px,${translateY}px,${translateZ}px)  rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+          const slideTransform = `translate3d(${translateX}px,${translateY}px,${translateZ}px)  rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 
           $slideEl.transform(slideTransform);
           $slideEl[0].style.zIndex = -Math.abs(Math.round(offsetMultiplier)) + 1;
-          // if (params.slideShadows) {
-          //   // Set shadows
-          //   let $shadowBeforeEl = isHorizontal ? $slideEl.find('.swiper-slide-shadow-left') : $slideEl.find('.swiper-slide-shadow-top');
-          //   let $shadowAfterEl = isHorizontal ? $slideEl.find('.swiper-slide-shadow-right') : $slideEl.find('.swiper-slide-shadow-bottom');
-          //   if ($shadowBeforeEl.length === 0) {
-          //     $shadowBeforeEl = swiper.$(`<div class="swiper-slide-shadow-${isHorizontal ? 'left' : 'top'}"></div>`);
-          //     $slideEl.append($shadowBeforeEl);
-          //   }
-          //   if ($shadowAfterEl.length === 0) {
-          //     $shadowAfterEl = swiper.$(`<div class="swiper-slide-shadow-${isHorizontal ? 'right' : 'bottom'}"></div>`);
-          //     $slideEl.append($shadowAfterEl);
-          //   }
-          //   if ($shadowBeforeEl.length) $shadowBeforeEl[0].style.opacity = offsetMultiplier > 0 ? offsetMultiplier : 0;
-          //   if ($shadowAfterEl.length) $shadowAfterEl[0].style.opacity = (-offsetMultiplier) > 0 ? -offsetMultiplier : 0;
-          // }
         }
 
          // Set correct perspective for IE10
@@ -257,8 +244,11 @@ export class StoriesComponent implements OnInit, OnDestroy {
           ws.perspectiveOrigin = `${center}px 50%`;
         }
       },
+
       setTransition(duration) {
         const swiper = this;
+        if(duration == 0)
+          duration = 300;
         swiper.slides
           .transition(duration)
           .find('.swiper-slide-shadow-top, .swiper-slide-shadow-right, .swiper-slide-shadow-bottom, .swiper-slide-shadow-left')
